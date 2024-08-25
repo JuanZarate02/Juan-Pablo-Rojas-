97e12e518511aadcb167b8dc8668e4d4532169ff
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:38:23 -0500

    Delete 05.ejercicio.py

--- 05.ejercicio.py
+++
@@ -1,7 +0,0 @@
-#Programa para calcular el 20% de cualquier número de entrada.
-
-num = int(input("ingrese un numero: "))
-
-resultado = num * 0.20
-
-print((f"el 20% del numero {num} es: {resultado}"))
