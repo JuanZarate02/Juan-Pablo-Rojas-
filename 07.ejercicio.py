43d09666ae178b6b8f47245e6def893cd36b823c
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:38:48 -0500

    Delete 07.ejercicio.py

--- 07.ejercicio.py
+++
@@ -1,7 +0,0 @@
-#Programa para calcular el área de un cuadrado, la longitud de un lado la ingresa el usuario.
-
-lado_cuadrado = int(input("Ingrese el valor del lado del cuadrado: "))
-
-area = lado_cuadrado ** 2
-
-print(f"el area del cuadrado es: {area}")
