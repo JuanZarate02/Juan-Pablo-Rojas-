7d0998bbd1d985dcb102a28116e55382bc704a77
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:40:29 -0500

    Delete 12.ejercicio.py

--- 12.ejercicio.py
+++
@@ -1,8 +0,0 @@
-#Calcular la hipotenusa con el Teorema de Pitágoras (sin usar funciones).
-
-a = float(input("Ingrese el valor del primer cateto: "))
-b = float(input("Ingrese el valor del segundo cateto: "))
-
-hipo = (a**2 + b**2) ** 0.5
-
-print(f"El valor de la hipotenusa es : {hipo}")
