7c1bf366a267eee705784b8ce4520c118d598f7f
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:36:03 -0500

    Delete 02.ejercicio.py

--- 02.ejercicio.py
+++
@@ -1,7 +0,0 @@
-#Solicitar al usuario un número y calcular cuál es el cuadrado del número (2^2=4).
-
-numero1 = int(input("ingrese un numero: "))
-
-resultado = numero1 ** 2
-
-print(f"el cuadrado del número {numero1} es: {resultado}")
