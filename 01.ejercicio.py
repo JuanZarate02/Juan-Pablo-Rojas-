ea2159efc523fd29605f85e2f0b4e4e502146f40
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:35:38 -0500

    Delete 01.ejercicio.py

--- 01.ejercicio.py
+++
@@ -1,9 +0,0 @@
-#Programa que permita multiplicar 3 números.
-
-numero1 = int(input("Ingrese el primer numero: "))
-numero2 = int(input("Ingrese el segundo numero: "))
-numero3 = int(input("Ingrese el tercer numero: "))
-
-resultado = numero1 * numero2 * numero3
-
-print(f"El resultado de la multiplicación es: {resultado}")
