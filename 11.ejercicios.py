1a5b8cb69dec24dd6174104e1b2548bc2228b63a
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:40:19 -0500

    Delete 11.ejercicios.py

--- 11.ejercicios.py
+++
@@ -1,7 +0,0 @@
-#Programa que solicite un número al usuario y permita calcular la raíz cuadrada del mismo (sin usar función).
-
-num = int(input("ingrese un numero: "))
-
-resultado = num ** 0.5
-
-print(f"el cuadrado del número {num} es: {resultado}")
