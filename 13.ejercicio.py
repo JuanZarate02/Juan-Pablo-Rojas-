161e1615442346d8597d3e6f44e4a6bd5391187d
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:40:39 -0500

    Delete 13.ejercicio.py

--- 13.ejercicio.py
+++
@@ -1,9 +0,0 @@
-#Solicitar tiempo en segundos y transformar a horas y minutos.
-
-t = int(input("Ingrese el tiempo en segundos: "))
-
-minu = t / 60
-horas = t / 3600
-
-print(f"El tiempo en minutos es: {minu}")
-print(f"El tiempo en minutos es: {horas}")
