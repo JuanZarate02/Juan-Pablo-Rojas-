a2da57c4add945f9f07ecd29667193ff746c1569
JuanZarate02 <juanzarater02@gmail.com>
2024-08-24 21:36:28 -0500

    Delete 04.ejercicio.py

--- 04.ejercicio.py
+++
@@ -1,8 +0,0 @@
-#Programa que permita calcular la edad de una persona conociendo el año actual y el usuario digita su año de nacimiento.
-
-año_actual = int(input("Ingrese el año actual: "))
-año_nacimiento = int(input("Ingrese el año de nacimiento: "))
-
-edad = año_actual - año_nacimiento
-
-print(f"la edad es: {edad}")
